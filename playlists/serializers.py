c309e355e33089368667cb5e7b543cbb99ce0179
DZavorotniy <dovakino98@gmail.com>
2022-07-11 19:07:33 +0300

    TUNE-14 Mapped Playlist model properties to Playlist serializer

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -3,6 +3,8 @@ from .models import Playlist
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
+    song_count = serializers.IntegerField()
+    total_duration = serializers.DurationField()
 
     class Meta:
         model = Playlist
