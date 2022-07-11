e8b4f38cafea84dee4d7ac3e4e78902ee242ad3f
DZavorotniy <dovakino98@gmail.com>
2022-07-11 18:22:05 +0300

    TUNE-5 Added mock model for tunes. Expanded the Playlist model

--- playlists/models.py
+++ playlists/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-from songs import Song
+from songs.models import Song
+from tuning.models import Tune
 
 
 class Playlist(models.Model):
-    pass
+    songs = models.ManyToManyField(Song)
+    tunes = models.ManyToManyField(Tune)
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def song_count(self):
+        return self.songs.all().count()
