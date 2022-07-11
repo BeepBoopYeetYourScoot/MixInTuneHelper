f9fb54c78378873bc7403c17fd560f6820c4a27a
DZavorotniy <dovakino98@gmail.com>
2022-07-11 18:57:15 +0300

    TUNE-8 Populated fields for Song model

--- songs/models.py
+++ songs/models.py
@@ -1,5 +1,23 @@
 from django.db import models
+from authors.models import Author
+from tuning.models import Tune
 
 
 class Song(models.Model):
-    pass
+    authors = models.ManyToManyField(Author, related_name='songs')
+    camelot_tune = models.ForeignKey(Tune, on_delete=models.DO_NOTHING, related_name='songs')
+
+    name = models.CharField(max_length=200)
+    tunebat_link = models.URLField()
+    bpm = models.PositiveSmallIntegerField(verbose_name='beats_per_minute')
+    duration = models.DurationField()
+
+    # Other characteristics of a song according to tunebat.com
+    popularity = models.PositiveSmallIntegerField()
+    energy = models.PositiveSmallIntegerField()
+    danceability = models.PositiveSmallIntegerField()
+    happiness = models.PositiveSmallIntegerField()
+    liveness = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return ', '.join(self.authors.all().values_list('name', flat=True)) + ' : ' + self.name
