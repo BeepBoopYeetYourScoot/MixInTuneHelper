aa3b3fa70ac4ffdeda0fb4e63c65b913336a3196
DZavorotniy <dovakino98@gmail.com>
2022-07-11 18:58:20 +0300

    TUNE-9 Expanded Playlist model

--- playlists/models.py
+++ playlists/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.db.models import Sum
+
 from songs.models import Song
 from tuning.models import Tune
 
 
 class Playlist(models.Model):
-    songs = models.ManyToManyField(Song)
-    tunes = models.ManyToManyField(Tune)
+    songs = models.ManyToManyField(Song, related_name='playlists')
+    tunes = models.ManyToManyField(Tune, related_name='playlists')
 
     name = models.CharField(max_length=200)
 
@@ -13,5 +15,9 @@ class Playlist(models.Model):
         return self.name
 
     @property
-    def song_count(self):
+    def song_count(self) -> int:
         return self.songs.all().count()
+
+    @property
+    def total_duration(self) -> dict[str: models.DurationField]:
+        return self.songs.all().aggregate(Sum('duration', output_field=models.DurationField()))
