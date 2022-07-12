92a8f1ad5a9170c62d0c2201264a8aefdfcaeb2a
DZavorotniy <dovakino98@gmail.com>
2022-07-12 19:47:25 +0300

    TUNE-19 Updated Song model to suit Spotify implementation. Added comments on how song features are mapped from Spotify API to the services' database

--- songs/models.py
+++ songs/models.py
@@ -7,17 +7,21 @@ class Song(models.Model):
     authors = models.ManyToManyField(Author, related_name='songs')
     camelot_tune = models.ForeignKey(Tune, on_delete=models.DO_NOTHING, related_name='songs')
 
-    name = models.CharField(max_length=200)
-    tunebat_link = models.URLField()
-    bpm = models.PositiveSmallIntegerField(verbose_name='beats_per_minute')
-    duration = models.DurationField()
+    name = models.CharField(max_length=200)  # song -> .
+    spotify_id = models.CharField(max_length=200)
 
-    # Other characteristics of a song according to tunebat.com
-    popularity = models.PositiveSmallIntegerField()
-    energy = models.PositiveSmallIntegerField()
-    danceability = models.PositiveSmallIntegerField()
-    happiness = models.PositiveSmallIntegerField()
-    liveness = models.PositiveSmallIntegerField()
+    # Other characteristics of a song according to Spotify
+
+    bpm = models.PositiveSmallIntegerField(verbose_name='beats_per_minute')  # features -> tempo
+    duration = models.DurationField()  # features -> duration_ms
+
+    popularity = models.PositiveSmallIntegerField()  # song -> .
+    energy = models.PositiveSmallIntegerField()  # features -> .
+    danceability = models.PositiveSmallIntegerField()  # features -> .
+    happiness = models.PositiveSmallIntegerField()  # features -> .
+    acousticness = models.PositiveSmallIntegerField()  # features -> .
+    instrumentalness = models.PositiveSmallIntegerField()  # features -> .
+    liveness = models.PositiveSmallIntegerField()  # features -> .
 
     def __str__(self):
         return ', '.join(self.authors.all().values_list('name', flat=True)) + ' : ' + self.name
